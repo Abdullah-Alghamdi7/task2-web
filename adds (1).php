91977ab89960ca8392f9a4c3fc794d6cd745a235
Abdullah Ahmed Alghamdi <139492658+Abdullah-Alghamdi7@users.noreply.github.com>
2023-08-07 23:18:44 +0300

    Add files via upload

---
+++ adds (1).php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "robot";
+
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+// $x=$_POST["x"];
+// $y=$_POST["y"];
+
+
+    $sql = "insert into moves (move,xdir,ydir)
+    values('stop','0','0')";
+
+
+
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
+
